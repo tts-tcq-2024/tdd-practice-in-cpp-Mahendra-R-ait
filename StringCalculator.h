bfb4f634e0c87f9e23ddfd1af3f6f83358b8326e
Mahendra R <83295768+Mahendra-R-ait@users.noreply.github.com>
2024-09-23 17:56:45 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,18 +1,16 @@
-#define STRINGCALCULATOR_H
+#ifndef STRINGCALCULATOR_H
 #define STRINGCALCULATOR_H
  
 #include <string>
 #include <cctype>
 #include <iostream>
-
-namespace std
  
 class StringCalculator
 {
   public:
     int less_than_thousand(int num);
     int get_positive_number(int num);
-    string processed_String_for_delimiters(string input);
-    int exception_handling_for_whitespce(string token);
-    int add(string input);
+    std::string processed_String_for_delimiters(std::string input);
+    int exception_handling_for_whitespce(std::string token);
+    int add(std::string input);
 };
